27bfb56c031a4f543220e105832f30a75a81e49e
Dominik Grabarczyk <s1873532@kostelic.inf.ed.ac.uk>
2019-03-01 14:55:33 +0000

    Updated main model

--- src/models.py
+++ src/models.py
@@ -106,3 +106,74 @@ class UNet32(nn.Module):
         #x = self.conv5(x)
         return x
 
+class UNet224(nn.Module):
+    def __init__(self):
+        super(UNet224, self).__init__()
+        #Convolution and deconvolution
+        self.conv1 = nn.Conv2d(1, 64, (4, 4), stride=2, padding=1)    # i = 224, o = 112
+        self.conv2 = nn.Conv2d(64, 128, (4, 4), stride=2, padding=1)   # i = 112, o = 56
+        self.conv3 = nn.Conv2d(128, 256, (4, 4), stride=2, padding=1) # i = 56, o = 28
+        self.conv4 = nn.Conv2d(256, 512, (4, 4), stride=2, padding=1) # i = 28, o = 14
+        self.conv5 = nn.Conv2d(512, 512, (4, 4), stride=2, padding=1) # i = 14, o = 7
+        self.conv6 = nn.Conv2d(512, 512, (4, 4), stride=1, padding=1) # i = 7, o = 5
+        self.conv7 = nn.Conv2d(512, 512, (4, 4), stride=1, padding=1) # i = 5, o = 3
+        
+        self.deconv1 = nn.ConvTranspose2d(512, 512, (4, 4), stride=1, padding=1)  # i = 3, o = 5
+        self.deconv2 = nn.ConvTranspose2d(1024, 512, (4, 4), stride=1, padding=1) # i = 5, o = 7
+        self.deconv3 = nn.ConvTranspose2d(1024, 512, (4, 4), stride=2, padding=1) # i = 7, o = 14
+        self.deconv4 = nn.ConvTranspose2d(1024, 256, (4, 4), stride=2, padding=1) # i = 14, o = 28
+        self.deconv2 = nn.ConvTranspose2d(512, 128, (4, 4), stride=2, padding=1)  # i = 28, o = 56
+        self.deconv3 = nn.ConvTranspose2d(256, 64, (4, 4), stride=2, padding=1)   # i = 56, o = 112
+        self.deconv4 = nn.ConvTranspose2d(128, 64, (4, 4), stride=2, padding=1)   # i = 112, o = 224
+        
+        self.conv5 = nn.Conv2d(64, 2, (1, 1))
+        
+        # RESUME HERE!!!
+        
+        #Batchnorm
+        self.conv1_bnorm = nn.BatchNorm2d(64)
+        self.conv2_bnorm = nn.BatchNorm2d(128)
+        self.conv3_bnorm = nn.BatchNorm2d(256)
+        self.conv4_bnorm = nn.BatchNorm2d(512)
+        
+        self.deconv1_bnorm = nn.BatchNorm2d(256)
+        self.deconv2_bnorm = nn.BatchNorm2d(128)
+        self.deconv3_bnorm = nn.BatchNorm2d(64)
+        self.deconv4_bnorm = nn.BatchNorm2d(64)
+    
+    def forward(self, x32):
+        # Contraction
+        x16 = F.leaky_relu(self.conv1(x32), 0.2)
+        x16 = self.conv1_bnorm(x16)
+        
+        x8 = F.leaky_relu(self.conv2(x16), 0.2)
+        x8 = self.conv2_bnorm(x8)
+        
+        x4 = F.leaky_relu(self.conv3(x8), 0.2)
+        x4 = self.conv3_bnorm(x4)
+        
+        x2 = F.leaky_relu(self.conv4(x4), 0.2)
+        x2 = self.conv4_bnorm(x2)
+        
+        
+        # Expansion
+        x = F.relu(self.deconv1(x2))
+        x = self.deconv1_bnorm(x)
+        x4 = torch.cat((x,x4), 1)
+        
+        x = F.relu(self.deconv2(x4))
+        x = self.deconv2_bnorm(x)
+        x8 = torch.cat((x,x8), 1)
+        
+        x = F.relu(self.deconv3(x8))
+        x = self.deconv3_bnorm(x)
+        x16 = torch.cat((x,x16), 1)
+        
+        x = F.relu(self.deconv4(x16))
+        x = self.deconv4_bnorm(x)
+        
+        # cross-channel parametric pooling
+        # CHECK IF TANH IS A GOOD IDEA???
+        x = F.tanh(self.conv5(x))
+        #x = self.conv5(x)
+        return x
