8a53c97a990da0c35acec0f4678bc43230fe99eb
akbokha <ak.bokharouss@gmail.com>
2019-03-04 13:45:01 +0000

    fix invalid args name in experiment scripts

--- scripts/experiment-base.sh
+++ scripts/experiment-base.sh
@@ -34,4 +34,4 @@ export DATASET_DIR=${TMP}/data/
 
 # Activate the relevant virtual environment:
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
-python /home/${STUDENT_ID}/image-colorization/train.py --experiment-name=experiment_001 --model=unet32 --dataset-name=cifar10 --train-batch-size=100 --val-batch-size=1000 --batch-output-frequency=10 --max-images=10
+python /home/${STUDENT_ID}/image-colorization/train.py --experiment-name=experiment_001 --model-name=unet32 --dataset-name=cifar10 --train-batch-size=100 --val-batch-size=1000 --batch-output-frequency=10 --max-images=10
