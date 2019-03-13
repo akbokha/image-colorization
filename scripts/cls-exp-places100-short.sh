6be8e113575d768d874d2b6d75720bcb942e3dbd
Simon Thorogood <simonthorogood@hotmail.com>
2019-03-13 10:25:16 +0000

    added short script

---
+++ scripts/cls-exp-places100-short.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Short
+#SBATCH --gres=gpu:4
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-03:59:00
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+# Activate the relevant virtual environment:
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+
+# Run script
+echo "Starting training."
+python /home/${STUDENT_ID}/image-colorization/train.py \
+    --task=classifier \
+    --experiment-name=cls_3 \
+    --dataset-name=places100 \
+    --use-dataset-archive=1
+    --model-path=/home/${STUDENT_ID}/models/ \
+    --train-batch-size=50 \
+    --val-batch-size=50 \
+    --batch-output-frequency=100 \
+    --max-epochs=5
