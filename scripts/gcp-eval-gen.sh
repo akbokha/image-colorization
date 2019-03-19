463b8fbc8fba2d05f59bf3d07319605a0398708a
Simon Thorogood <simonthorogood@hotmail.com>
2019-03-19 21:09:47 +0000

    updated script

--- scripts/gcp-eval-gen.sh
+++ scripts/gcp-eval-gen.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Create images from test dataset for evaluation
+# Weights are quantitative selection
 
 cd ..
 
@@ -23,14 +24,34 @@ python train.py \
     --batch-output-frequency=1 \
 
 python train.py \
-    --experiment-name=eval_gen_resnet \
+    --experiment-name=eval_gen_resnet_quant \
     --task=eval-gen \
     --eval-type=colorized \
     --model-name=resnet \
-    --model-suffix=073 \
+    --model-suffix=091 \
     --dataset-name=places100 \
     --train-batch-size=25 \
     --val-batch-size=25 \
     --batch-output-frequency=1 \
 
-# TODO: Add UNet and GAN
+python train.py \
+    --experiment-name=eval_gen_unet_quant \
+    --task=eval-gen \
+    --eval-type=colorized \
+    --model-name=unet \
+    --model-suffix=059 \
+    --dataset-name=places100 \
+    --train-batch-size=25 \
+    --val-batch-size=25 \
+    --batch-output-frequency=1 \
+
+python train.py \
+    --experiment-name=eval_gen_cgan_quant \
+    --task=eval-gen \
+    --eval-type=colorized \
+    --model-name=cgan \
+    --model-suffix=042 \
+    --dataset-name=places100 \
+    --train-batch-size=25 \
+    --val-batch-size=25 \
+    --batch-output-frequency=1 \
