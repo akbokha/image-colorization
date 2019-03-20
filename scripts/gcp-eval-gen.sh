52f465a145d9a31fa243f2edad5e2e575bef704a
Simon Thorogood <simonthorogood@hotmail.com>
2019-03-20 11:02:15 +0000

    updated script

--- scripts/gcp-eval-gen.sh
+++ scripts/gcp-eval-gen.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
 # Create images from test dataset for evaluation
-# Weights are quantitative selection
 
 cd ..
 
-python train.py \
-    --experiment-name=eval_gen_original \
-    --task=eval-gen \
-    --eval-type=original \
-    --dataset-name=places100 \
-    --train-batch-size=25 \
-    --val-batch-size=25 \
-    --batch-output-frequency=1 \
-
-python train.py \
-    --experiment-name=eval_gen_grayscale \
-    --task=eval-gen \
-    --eval-type=grayscale \
-    --dataset-name=places100 \
-    --train-batch-size=25 \
-    --val-batch-size=25 \
-    --batch-output-frequency=1 \
+#python train.py \
+#    --experiment-name=eval_gen_original \
+#    --task=eval-gen \
+#    --eval-type=original \
+#    --dataset-name=places100 \
+#    --train-batch-size=25 \
+#    --val-batch-size=25 \
+#    --batch-output-frequency=1 \
+#
+#python train.py \
+#    --experiment-name=eval_gen_grayscale \
+#    --task=eval-gen \
+#    --eval-type=grayscale \
+#    --dataset-name=places100 \
+#    --train-batch-size=25 \
+#    --val-batch-size=25 \
+#    --batch-output-frequency=1 \
 
 python train.py \
     --experiment-name=eval_gen_resnet_quant \
@@ -35,11 +34,33 @@ python train.py \
     --batch-output-frequency=1 \
 
 python train.py \
+    --experiment-name=eval_gen_resnet_qual \
+    --task=eval-gen \
+    --eval-type=colorized \
+    --model-name=resnet \
+    --model-suffix=073 \
+    --dataset-name=places100 \
+    --train-batch-size=25 \
+    --val-batch-size=25 \
+    --batch-output-frequency=1 \
+
+python train.py \
     --experiment-name=eval_gen_unet_quant \
     --task=eval-gen \
     --eval-type=colorized \
     --model-name=unet \
-    --model-suffix=059 \
+    --model-suffix=087 \
+    --dataset-name=places100 \
+    --train-batch-size=25 \
+    --val-batch-size=25 \
+    --batch-output-frequency=1 \
+
+python train.py \
+    --experiment-name=eval_gen_unet_qual \
+    --task=eval-gen \
+    --eval-type=colorized \
+    --model-name=unet \
+    --model-suffix=078 \
     --dataset-name=places100 \
     --train-batch-size=25 \
     --val-batch-size=25 \
@@ -51,6 +72,17 @@ python train.py \
     --eval-type=colorized \
     --model-name=cgan \
     --model-suffix=042 \
+    --dataset-name=places100 \
+    --train-batch-size=25 \
+    --val-batch-size=25 \
+    --batch-output-frequency=1 \
+
+python train.py \
+    --experiment-name=eval_gen_cgan_qual \
+    --task=eval-gen \
+    --eval-type=colorized \
+    --model-name=cgan \
+    --model-suffix=028 \
     --dataset-name=places100 \
     --train-batch-size=25 \
     --val-batch-size=25 \
